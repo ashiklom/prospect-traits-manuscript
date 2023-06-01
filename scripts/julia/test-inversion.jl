0313ef1e43535f5ce2d3b74cff31f84b525f55cc
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-06-01 18:03:55 -0400

    Move test-arrow.jl to test-inversion.jl

--- scripts/julia/test-inversion.jl
+++ scripts/julia/test-inversion.jl
@@ -1,21 +1,31 @@
-# import Pkg; Pkg.activate("ProspectTraits.jl")
+import Pkg; Pkg.activate(".")
 using Revise
 
 using ProspectTraits
-using CSV
+
+using Arrow
 using DataFrames
+
 using Unitful
 
-lopex_01 = CSV.read(
-    "../../spectra_db/lopex/spectra/lopex_01.csvy",
-    DataFrame;
-    comment = "#"
-)
+using Serialization
+using Base.Filesystem
 
-obs = Spectrum(
-    lopex_01[:,"wavelengths"]u"nm",
-    Array(select(lopex_01, Not(:wavelengths)))
-    # lopex_01[:,"lopex_01.001"]
-)
+data_basedir = "data/"
+dataset_id = "lopex"
 
-result = ProspectTraits.fit_prospect(obs, 5000)
+const metadata = DataFrame(Arrow.Table("$data_basedir/ecosis-processed/$dataset_id/metadata.arrow"))
+const spectra_data = DataFrame(Arrow.Table("$data_basedir/ecosis-processed/$dataset_id/spectra.arrow"))
+
+function fit_row(metadata, spectra_data, rn; nsamp = 500)
+    observation_id = metadata[rn, :observation_id]
+    observation = as_spectrum(spectra_data, observation_id)
+    return fit_prospectpro(observation, nsamp)
+end
+
+# 249.54 seconds
+r1 = fit_row(metadata, spectra_data, 1; nsamp = 500)
+
+using StatsPlots
+plot(r1)
+
