547d9c033830fe0819f56ba3b67fed449eb01c1d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2023-09-29 17:23:45 -0400

    Use Progressbars in summarize

--- scripts/julia/summarize-results.jl
+++ scripts/julia/summarize-results.jl
@@ -3,9 +3,17 @@ import Pkg; Pkg.activate(".")
 
 using ProspectTraits
 using Glob
+using Serialization
+
+using ProgressBars
 
 resultfiles = glob("*/*/*.result", "data/results/raw")
 
-summaries = [summarize_fit(file) for file in resultfiles]
+iter = ProgressBar(resultfiles)
+summaries = [summarize_fit(file) for file in iter]
+
 params = vcat([s[1] for s in summaries]...)
 corrs = vcat([s[2] for s in summaries]...)
+
+serialize("data/results/params", params)
+serialize("data/results/corrs", corrs)
